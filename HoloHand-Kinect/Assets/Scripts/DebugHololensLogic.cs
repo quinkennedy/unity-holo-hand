47ae4e882d6240cca431927700f16ba1fb2d7648
Quin Kennedy <quinkennedy@gmail.com>
2017-01-07 13:44:29 -0800

    added debug hololens prefab for testing without connecting a remote hololens

---
+++ HoloHand-Kinect/Assets/Scripts/DebugHololensLogic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugHololensLogic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        KinectRegistration.closestHMD = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
