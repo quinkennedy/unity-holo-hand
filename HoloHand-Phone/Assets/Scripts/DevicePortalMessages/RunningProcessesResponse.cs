c998bc998c7eaf405b3c3ee32d9f5635f0d955c0
Oregon Story Board <Oregon Story Board>
2017-01-11 20:32:37 -0800

    added objects to deserialize more device portal responses

---
+++ HoloHand-Phone/Assets/Scripts/DevicePortalMessages/RunningProcessesResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RunningProcessesResponse {
+    public Process[] Processes;
+
+    [System.Serializable]
+    public class Process
+    {
+        public float CPUUsage;
+        public string ImageName;
+        public int PageFileUsage;
+        public int PrivateWorkingSet;
+        public int ProcessId;
+        public int SessionId;
+        public int TotalCommit;
+        public string UserName;
+        public int VirtualSize;
+        public int WorkingSetSize;
+    }
+
+    public static RunningProcessesResponse CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<RunningProcessesResponse>(jsonString);
+    }
+}
---
+++ HoloHand-Phone/Assets/Scripts/DevicePortalMessages/ThermalStageResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ThermalStageResponse {
+    public int CurrentStage;
+
+    public static ThermalStageResponse CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<ThermalStageResponse>(jsonString);
+    }
+}
