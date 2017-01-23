f6b1893b7c20c1aee96a6ca5d68f9f5e83f0bdd0
Quin Kennedy <quinkennedy@gmail.com>
2017-01-23 15:59:20 -0700

    using Unity deserialization for Kinect config

--- HoloHand-Kinect/Assets/Scripts/KinectConfig.cs
+++ HoloHand-Kinect/Assets/Scripts/KinectConfig.cs
@@ -3,104 +3,84 @@ using System.Collections;
 using System.IO;
 using System.Collections.Generic;
 
+[System.Serializable]
 public class KinectConfig
 {
-
-    public struct Box
-    {
-        public Vector3 position;
-        public Vector3 scale;
-        public Vector3 rotation;
-
-        public Box(Vector3 position, Vector3 scale, Vector3 rotation)
-        {
-            this.position = position;
-            this.scale = scale;
-            this.rotation = rotation;
-        }
-    }
-    
-    public Vector3 kinect_pos;
-    public Vector3 kinect_rot;
-    public Vector3 kinect_bounds_pos;
-    public Vector3 kinect_bounds_scale;
+    public float kinect_height;
+    public int kinect_threshold = 3000;
+    public float kinect_distance = 2.5f;
+    public float[] kinect_rot;
+    public float[] kinect_anchor_offset;
+    public float[] kinect_bounds_pos;
+    public float[] kinect_bounds_scale;
     public Box HMD_active_area;
     public List<Box> buttons;
-    
-    public int depthTheshold = 3000;
 
-    public float depthDistance = 2.5f;
+    [System.Serializable]
+    public struct Box
+    {
+        public float[] position;
+        public float[] scale;
+        public float[] rotation;
 
+        //Getters
+        public Vector3 Position
+        {
+            get { return new Vector3(position[0], position[1], position[2]); }
+        }
+        public Vector3 Scale
+        {
+            get { return new Vector3(scale[0], scale[1], scale[2]); }
+        }
+        public Vector3 Rotation
+        {
+            get { return new Vector3(rotation[0], rotation[1], rotation[2]); }
+        }
+    }
 
-    public KinectConfig( string path)
+    //Getters
+    public float KinectHeight
+    {
+        get { return kinect_height; }
+    }
+    public int KinectThreshold
+    {
+        get { return kinect_threshold; }
+    }
+    public float KinectDistance
+    {
+        get { return kinect_distance; }
+    }
+    public Vector3 KinectRotation
+    {
+        get { return new Vector3(kinect_rot[0], kinect_rot[1], kinect_rot[2]); }
+    }
+    public Vector3 KinectAnchor
+    {
+        get { return new Vector3(kinect_anchor_offset[0], kinect_anchor_offset[1], kinect_anchor_offset[2]); }
+    }
+    public Vector3 KinectBoundsPosition
+    {
+        get { return new Vector3(kinect_bounds_pos[0], kinect_bounds_pos[1], kinect_bounds_pos[2]); }
+    }
+    public Vector3 KinectBoundsScale
+    {
+        get { return new Vector3(kinect_bounds_scale[0], kinect_bounds_scale[1], kinect_bounds_scale[2]); }
+    }
+
+    public static KinectConfig CreateFromJSON(string path)
     {
         try
         {
             string configJson = File.ReadAllText(path);
-            processConfig(configJson);
+            return JsonUtility.FromJson<KinectConfig>(configJson);
 
         }
-        catch ( System.Exception e)
+        catch (System.Exception e)
         {
             Debug.LogError("Error loading config.json -- " + e.Message);
             Debug.LogError(e.StackTrace);
+            return null;
         }
     }
-
-    private Box parseBox(JSONObject box)
-    {
-        List<JSONObject> jPos = box["pos"].list;
-        Vector3 pos = new Vector3(float.Parse(jPos[0].ToString()),
-                                  float.Parse(jPos[1].ToString()),
-                                  float.Parse(jPos[2].ToString()));
-        List<JSONObject> jScale = box["scale"].list;
-        Vector3 scale = new Vector3(float.Parse(jScale[0].ToString()),
-                                    float.Parse(jScale[1].ToString()),
-                                    float.Parse(jScale[2].ToString()));
-        List<JSONObject> jRot = box["rot"].list;
-        Vector3 rot = new Vector3(float.Parse(jRot[0].ToString()),
-                                  float.Parse(jRot[1].ToString()),
-                                  float.Parse(jRot[2].ToString()));
-        return new global::KinectConfig.Box(pos, scale, rot);
-    }
-
-    public void processConfig(string jsondata)
-    {
-        JSONObject obj = new JSONObject(jsondata);
-
-        JSONObject j = obj[0];
-        
-        j.GetField(ref depthTheshold, "kinect_threshold");
-
-        j.GetField(ref depthDistance, "kinect_distance");
-        
-
-        kinect_pos = new Vector3( float.Parse(j["kinect_position"].list[0].ToString()),
-                                    float.Parse(j["kinect_position"].list[1].ToString()),
-                                    float.Parse(j["kinect_position"].list[2].ToString()));
-
-        kinect_rot = new Vector3(float.Parse(j["kinect_rotation"].list[0].ToString()),
-                                    float.Parse(j["kinect_rotation"].list[1].ToString()),
-                                    float.Parse(j["kinect_rotation"].list[2].ToString()));
-
-        kinect_bounds_pos = new Vector3(float.Parse(j["kinect_bounds_pos"].list[0].ToString()),
-                                    float.Parse(j["kinect_bounds_pos"].list[1].ToString()),
-                                    float.Parse(j["kinect_bounds_pos"].list[2].ToString()));
-
-        kinect_bounds_scale = new Vector3(float.Parse(j["kinect_bounds_scale"].list[0].ToString()),
-                                    float.Parse(j["kinect_bounds_scale"].list[1].ToString()),
-                                    float.Parse(j["kinect_bounds_scale"].list[2].ToString()));
-
-        buttons = new List<Box>();
-        if (j.HasField("buttons")) {
-            List<JSONObject> jButtons = j["buttons"].list;
-            for (int i = 0; i < jButtons.Count; i++)
-            {
-                buttons.Add(parseBox(jButtons[i]));
-            }
-        }
-
-        HMD_active_area = parseBox(j["hmd_active_area"]);
-    }
-
 }
