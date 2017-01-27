293205b1bfbcc0270da53a3af3c1c68379a6f39d
Quin Kennedy <quinkennedy@gmail.com>
2017-01-26 17:32:20 -0700

    creating commands to dynamically add/remove anchors

---
+++ HoloHand-Lens/Assets/Scripts/HololensLogic.cs
@@ -0,0 +1,34 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HololensLogic : MonoBehaviour {
+
+    public GameObject worldLabel;
+    private List<GameObject> anchors = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void AddAnchor()
+    {
+
+        if (WorldAnchorManager.Instance != null)
+        {
+            WorldAnchorManager.Instance.AttachAnchor(CalibrationPlane.gameObject, ObjectAnchorStoreName);
+        }
+        else
+        {
+            Debug.LogWarning("[HololensAvatarLogic:Start] WorldAnchorManager.Instance is null");
+        }
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
